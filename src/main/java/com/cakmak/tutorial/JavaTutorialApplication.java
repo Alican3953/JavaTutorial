1bb479704c149c29aa6e83265d9f4ca40474881d
Mükail Çakmak <mukail.cakmak@cmvteknoloji.com>
2023-07-07 17:50:42 +0300

    [AlicanKarakus] deneme amaclı

--- src/main/java/com/cakmak/tutorial/JavaTutorialApplication.java
+++ src/main/java/com/cakmak/tutorial/JavaTutorialApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaTutorialApplication {
-
+	//Main Class
 	public static void main(String[] args) {
 		SpringApplication.run(JavaTutorialApplication.class, args);
 	}
