a0c6c5fb4ba0d34f8eb48cd3205e2aa4ccba3f79
Alican3953 <121888775+Alican3953@users.noreply.github.com>
2023-08-02 22:34:14 +0300

    Add files via upload

---
+++ 1 girildiğinde sayıları b k 0 girildiğinde k b/src/main/java/com/say/lar/s/ralama/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.say.lar.s.ralama.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+@SpringBootApplication
+public class DemoApplication {
+
+
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
